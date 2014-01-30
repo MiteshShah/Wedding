b07f807206c859c8cc199a7d7ee193ee4189b5ec
Mitesh Shah <Mitesh.Shah@rtcamp.com>
2014-01-30 11:57:42 +0530

    Sudo Invite Friends

---
+++ Wedding.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+
+#words="Hello Friends"
+#while test -n "$words"; do
+#   c=${words:0:1}     # Get the first character
+#   echo -n $c
+#   sleep 0.1s;
+#   words=${words:1}   # trim the first character
+#done
+clear
+function typewriter
+{
+    text="$1"
+    delay="$2"
+    
+    for i in $(seq 0 $(expr length "${text}")) ; do
+        echo -n "${text:$i:1}"
+        sleep ${delay}
+    done
+}
+
+#NAME=$(whoami | sed 's/.*/\u&/')
+#if [ $NAME = "Root" ] || [ $NAME = "Rtcamp" ] || [ $NAME = "Www-data" ]
+#then
+#	read -p "Enter Your Name: " ASKNAME
+#	NAME=$(echo $ASKNAME | sed 's/.*/\u&/')
+#fi
+#typewriter "Hello $(echo $NAME)," .1
+echo
+echo
+typewriter "		Two lives, two hearts joined together in friendship" .1
+echo
+typewriter "		united forever in love It is with joy that we," .1
+echo
+echo
+typewriter "				Mitesh Shah" .1
+echo
+typewriter "			    	    and" .1
+echo
+typewriter "				Hetal Shah" .1
+echo
+echo
+typewriter "		invite you to share in a celebration of love" .1
+echo
+typewriter "		as we exchange our marriage vows at" .1
+echo
+echo
+typewriter "Wedding Ceremony:				Wedding Party:" .1
+echo
+typewriter "Time:  12:39pm					Time:  12:30pm" .1
+echo
+typewriter "Date:  Monday, 3rd February 2014		Date:  Monday, 4th February 2014" .1
+echo
+typewriter "Venue: Kotyark Temple,				Venue: A-106, Royal Residency," .1
+echo
+typewriter "       Mahudi, Gandhinagar			       Visnagar, Mehsana" .1
+echo
+typewriter "       http://goo.gl/maps/2dUvJ			       http://goo.gl/maps/Pfgex" .1
+echo
+echo
+echo # <-- Just for a newline
